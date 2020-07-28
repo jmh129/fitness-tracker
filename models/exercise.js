7ca8be3f1210eb6e036e2da7048141f574fde44f
James Holding <james.holding90@gmail.com>
2020-07-27 22:38:58 -0400

    app seems to be working fine.

--- models/exercise.js
+++ models/exercise.js
@@ -1,16 +1,16 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema;
+// const Schema = mongoose.Schema;
 
-const exerciseSchema = new Schema({
-  type: String,
-  name: String,
-  duration: Number,
-  weight: Number,
-  reps: Number,
-  sets: Number,
-});
+// const exerciseSchema = new Schema({
+//   type: String,
+//   name: String,
+//   duration: Number,
+//   weight: Number,
+//   reps: Number,
+//   sets: Number,
+// });
 
-const Exercise = mongoose.model("Exercise", exerciseSchema);
+// const Exercise = mongoose.model("Exercise", exerciseSchema);
 
-module.exports = Exercise;
+// module.exports = Exercise;
